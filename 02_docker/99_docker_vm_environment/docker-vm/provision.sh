24d9171488e10a39d0940d5a439aeefd9daf95f3
Jaime Salas Zancada <jaimesalaszancada@Jaimes-MacBook-Pro.local>
2021-06-02 19:17:13 +0200

    added provision script for docler compose

---
+++ 02_docker/99_docker_vm_environment/docker-vm/provision.sh
@@ -0,0 +1,5 @@
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
+
+echo $(docker-compose --version)
